f103d2e66061a752321ae9c2297b21e0324130e1
techryota <qxnsz54@gmail.com>
2019-11-11 14:05:51 +0900

    user-function

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root 'users#edit'
+  resources :users, only: [:edit, :update]
 end
