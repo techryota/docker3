3e345a3b53f046294f6bcfd048a98e411fdb080b
techryota <qxnsz54@gmail.com>
2020-01-07 16:48:30 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock '3.11.2'
 set :application, 'docker3'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:akasakaa/docker3.git'
+set :repo_url,  'git@github.com:techryota/docker3.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
