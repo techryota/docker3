a7cc43716bb3c28ad25a26663a767af8de969f0e
techryota <qxnsz54@gmail.com>
2020-03-06 13:17:20 +0900

    Update counts_controller_spec.rb

--- spec/controllers/counts_controller_spec.rb
+++ spec/controllers/counts_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 describe CountsController do
-
+  let(:user) { create(:user) }
+  let(:group) { create(:group, user: user) }
+  describe 'GET #index' do
+    before do
+      login_user user
+      # get :index, params: { group_id: group.id }
+    end
+    it 'redners index' do
+      count = Count.new
+      get :index, params: { group_id: group.id }
+      expect(assigns(:count)).to eq count
+    end
+  end
 end
